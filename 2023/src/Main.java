4fb14742d78b6d2a86b76e69e1c7e5395721bc52
powersindustries <lp.accounts@fastmail.com>
2023-12-04 22:03:13 -0600

    Added solution for day 1 2023.

--- 2023/src/Main.java
+++ 2023/src/Main.java
@@ -7,6 +7,7 @@ public class Main {
         System.out.println("Advent of Code Solutions for 2023.");
 
         Day1 day1 = new Day1();
+        System.out.println("Day 1");
         System.out.println("Part 1: " + day1.Part1());
         System.out.println("Part 2: " + day1.Part2());
 
--- 2023/src/Solutions/Day1.java
+++ 2023/src/Solutions/Day1.java
@@ -1,13 +1,140 @@
 package Solutions;
 
+import java.io.*;
+import java.net.URL;
+import java.util.*;
+
 public class Day1 {
 
+    private URL dataPath = getClass().getResource("../Data/day1.txt");
+    private List<String> numberList = new ArrayList<String>();
+
+    public Day1() {
+        numberList.add("one");
+        numberList.add("two");
+        numberList.add("three");
+        numberList.add("four");
+        numberList.add("five");
+        numberList.add("six");
+        numberList.add("seven");
+        numberList.add("eight");
+        numberList.add("nine");
+    }
+
+
     public int Part1() {
-        return 0;
+        int output = 0;
+
+        try {
+            Scanner scanner = new Scanner(new File(dataPath.getPath()));
+
+            while (scanner.hasNextLine()) {
+                String currLine = scanner.nextLine();
+
+                // Get all number.
+                List<Character> numberList = new ArrayList<Character>();
+                for (int x = 0; x < currLine.length(); ++x) {
+                    if (Character.isDigit(currLine.charAt(x))) {
+                        numberList.add(currLine.charAt(x));
+                    }
+                }
+
+                if (numberList.size() == 0) {
+                    continue;
+                } else if (numberList.size() > 1) {
+                    String outputString = "" + numberList.get(0) + numberList.get(numberList.size() - 1);
+                    output += Integer.parseInt(outputString);
+                } else {
+                    String outputString = "" + numberList.get(0) + numberList.get(0);
+                    output += Integer.parseInt(outputString);
+                }
+
+            }
+
+            scanner.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return output;
     }
 
     public int Part2() {
-        return 0;
+        int output = 0;
+
+        try {
+            Scanner scanner = new Scanner(new File(dataPath.getPath()));
+
+            while (scanner.hasNextLine()) {
+                String currLine = scanner.nextLine();
+
+                // Get all number.
+                List<Character> numberList = new ArrayList<Character>();
+                for (int x = 0; x < currLine.length(); ++x) {
+                    if (Character.isDigit(currLine.charAt(x))) {
+
+                        // Check if word number exists at beginning of string.
+                        if (numberList.size() == 0) {
+                            String substring = currLine.substring(0, x);
+                            char characterToNumber = substringIncludesNumber(substring);
+                            if(characterToNumber != Character.MIN_VALUE){
+                                numberList.add(characterToNumber);
+                            }
+                        }
+
+                        numberList.add(currLine.charAt(x));
+                    }
+                }
+
+                // Check if word number exists at the end of string.
+                for (int x = currLine.length() - 1; x > 0; --x) {
+                    if (Character.isDigit(currLine.charAt(x))) {
+
+                        // Check if word number exists at beginning of string.
+                        if (numberList.size() == 0) {
+                            String substring = currLine.substring(0, x);
+                            char characterToNumber = substringIncludesNumber(substring);
+                            if(characterToNumber != Character.MIN_VALUE){
+                                numberList.add(characterToNumber);
+                            }
+                        }
+
+                        numberList.add(currLine.charAt(x));
+                    }
+                }
+
+                if (numberList.size() == 0) {
+                    continue;
+                } else if (numberList.size() > 1) {
+                    String outputString = "" + numberList.get(0) + numberList.get(numberList.size() - 1);
+                    output += Integer.parseInt(outputString);
+                } else {
+                    String outputString = "" + numberList.get(0) + numberList.get(0);
+                    output += Integer.parseInt(outputString);
+                }
+
+            }
+
+            scanner.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return output;
     }
+
+    private char substringIncludesNumber(String inString) {
+
+        for (int x = 0; x < numberList.size(); ++x) {
+            if (inString.indexOf(numberList.get(x)) > 0) {
+                return (char) (x + 1);
+            }
+        }
+
+        return Character.MIN_VALUE;
+    }
+
 
 }
